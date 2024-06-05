67783495fbee5e86cd61a423c3d5203ea7242625
Jordan Gidman <jordanleegidman@hotmail.com>
2024-06-05 14:09:09 +0100

    Supabase integration
    I have set up a basic back-end using supabase and implemented the basic structure creating tables to start with for the bookings, cabins, guests and settings. There will be more later but its a start.
    
    I have also then connected the database with my react app using the supabase API, which is incredibly user friendly, and can now read the data through my app.

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://swnljlgucehtzgluhzxp.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3bmxqbGd1Y2VodHpnbHVoenhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc1ODkwMzgsImV4cCI6MjAzMzE2NTAzOH0._ZwhWj9a1YFYdXKXGUGZ4wYAqyv9VaAb3n8QPNoPfc4";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
