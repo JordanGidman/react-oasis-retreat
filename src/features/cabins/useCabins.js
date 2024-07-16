bb91853e71d2c0c88e851c6b1bf3934aed9c3ae6
Jordan Gidman <jordanleegidman@hotmail.com>
2024-07-16 16:10:59 +0100

    Filtering and Sorting
    Added functionality for filtering the cabins. I tried to make this very re-usable both for use later in this project and with another that i will soon be starting. For this i have made a filter styled component which takes in a field name and an array of option objects from which it will render the buttons for the filtering i.e. filtering by discounts. I am accessing which option was chosen by using the useSearchParams hook to get the necessary info from the URL.
    
    To couple with the sorting feature i also added a means of sorting the cabins based on a few different things much the same way as the filtering to be reusable.

--- src/features/cabins/useCabins.js
+++ src/features/cabins/useCabins.js
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getCabins } from "../../services/apiCabins";
 
 export function useCabins() {
+  // The useQuery hook needs an object with both a queryKey which will identify the data which needs to be an array. And the queryFn which is the function to actually do the querying.
   const {
     isLoading,
     data: cabins,
