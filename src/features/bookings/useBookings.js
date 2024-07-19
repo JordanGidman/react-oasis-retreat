33b3d52e14b62f61066bb0e7b5b6411821f3aa5f
Jordan Gidman <jordanleegidman@hotmail.com>
2024-07-19 15:04:17 +0100

    Server side filtering and sorting
    Today i have implemented the bookings table and used a component for development to upload some dummy table data for me to work with.
    
    I have also implemented the filter and sort components that were used on the cabin table here but re-worked them to do the filtering on the server side as here we dont want to get more data from the database than we need to improve optimization. Thankfully supabase is incredibly intuitive so this is no problem, we simply tailor the query string as needed and let react query re-fetch the necessary data. I ran into an issue where it was not triggering a re-render upon the data change. I figured out that query gives me a solution for this as well to give it a dependency array essentially, telling it to refetch and thus re-render the page whenever the filter or sortBy fields change.

---
+++ src/features/bookings/useBookings.js
@@ -0,0 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
+
+export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  //Filter
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : {
+          field: "status",
+          value: filterValue,
+        };
+
+  //Sort
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+  // The useQuery hook needs an object with both a queryKey which will identify the data which needs to be an array. And the queryFn which is the function to actually do the querying.
+  //The queryKey has more than one argument to give query a dependancy array and ensure that react Query will re-fetch the data and re-render the page when the user filters by something as it does not on its own
+  const {
+    isLoading,
+    data: bookings,
+    error,
+  } = useQuery({
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
+  });
+
+  return { isLoading, error, bookings };
+}
